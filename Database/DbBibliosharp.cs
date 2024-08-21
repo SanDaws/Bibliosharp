3ae20a95b341a44f3043e6eb00b8d09090312c0e
Daws <castrogiraldosantiago@gmail.com>
2024-08-20 19:20:03 -0500

    Feat: db base class import

--- Database/DbBibliosharp.cs
+++ Database/DbBibliosharp.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Bibliosharp.Database;
-    public class DbBibliosharp
+    public class DbBibliosharp : DbContext
     {
         
     }
