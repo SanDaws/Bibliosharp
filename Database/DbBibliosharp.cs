a84a93e129c8ae2a7b10407e78c2284474d94dcf
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-22 08:41:33 -0500

    Feat: all entity added

--- Database/DbBibliosharp.cs
+++ Database/DbBibliosharp.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bibliosharp.Models;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace Bibliosharp.Database;
     public class DbBibliosharp : DbContext
     {
-        
+        public DbSet<Author> entAuthors; 
+        public DbSet<Book> entBooks; 
+        public DbSet<BookHRecord> entBookHRecords; 
+        public DbSet<Category> entCategory; 
+        public DbSet<DocumentType> entDocumentTypes; 
+        public DbSet<Rol> entRoles; 
+        public DbSet<User> entUsers; 
+        public DbBibliosharp(DbContextOptions options) : base(options)
+        {
+        }
+
     }
