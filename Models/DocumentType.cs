2a266343d15598e680b724f7016e037eab2ef3f3
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-22 11:02:18 -0500

    Hotfix: brackets clousure

--- Models/DocumentType.cs
+++ Models/DocumentType.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Bibliosharp.Models
-{
+namespace Bibliosharp.Models;
     public class DocumentType
     {
         public int Id { get; set; }
