34dfc37efdd236602a2f4ec2aac8f2caf3cc03cf
Tribog <julix4212@gmail.com>
2024-08-22 10:50:01 -0500

    feat: class DocumentType defining

--- Models/DocumentType.cs
+++ Models/DocumentType.cs
@@ -7,6 +7,16 @@ namespace Bibliosharp.Models
 {
     public class DocumentType
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abreviation { get; set; }
+
+        public DocumentType(string name, string abreviation)
+    {
+        Name = name;
+       Abreviation = abreviation;
     }
 }
+    }
+
+    
