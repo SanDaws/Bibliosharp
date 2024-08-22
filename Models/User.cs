5e0350d7f718aed3dcd9c7bbde4bf377985d5b88
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-22 10:39:56 -0500

    Feat:  constructors

--- Models/User.cs
+++ Models/User.cs
@@ -7,18 +7,34 @@ namespace Bibliosharp.Models
 {
     public class User : Person
     {
+        //Attributes
         public int Rol;
         private int DocumentType;
         private string DocumentNumber;
-        private string address;
-        private string phone;
-        private string email;
-        private string password;
+        private string Address;
+        private string Phone;
+        private string Email;
+        private string Password;
+        //constructors 
         public User(string firstName, string secondName, string firstLastName, string secondLastName,string addres, string phone,string email, string password ,Rol Rol,DocumentType DocumentType ) 
         : base(firstName, secondName, firstLastName, secondLastName)
         {
             this.Rol= Rol.Id;
-            
+            this.DocumentType= DocumentType.Id;
+            Address=addres;
+            Phone=phone;
+            Email=email;
+            Password=password;
+        }
+        public User(string firstName, string secondName, string firstLastName, string secondLastName,string addres, string phone,string email, string password ,int rol,int documentType ) 
+        : base(firstName, secondName, firstLastName, secondLastName)
+        {
+            this.Rol= rol;
+            this.DocumentType= documentType;
+            Address=addres;
+            Phone=phone;
+            Email=email;
+            Password=password;
         }
     }
 }
