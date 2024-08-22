5a47de3fae980c0c8c4ca1c95929c790373c79c0
Tribog <julix4212@gmail.com>
2024-08-22 11:08:05 -0500

    feat:  class RolName defined

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Bibliosharp.Models
-{
+namespace Bibliosharp.Models;
+
     public class Category
     {
-        
+        public int Id { get; set; }
+        public string RolName { get; set; }
+
+        public Category(string rolName)
+        {
+            RolName= rolName;
+        }
     }
-}
+
