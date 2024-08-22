9e499de4db4050416dd016174f6e86dbdf679db2
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-22 10:13:51 -0500

    Feat: attributes definitions

--- Models/User.cs
+++ Models/User.cs
@@ -5,8 +5,20 @@ using System.Threading.Tasks;
 
 namespace Bibliosharp.Models
 {
-    public class User
+    public class User : Person
     {
-        
+        public int Rol;
+        private int DocumentType;
+        private string DocumentNumber;
+        private string address;
+        private string phone;
+        private string email;
+        private string password;
+        public User(string firstName, string secondName, string firstLastName, string secondLastName,string addres, string phone,string email, string password ,Rol Rol,DocumentType DocumentType ) 
+        : base(firstName, secondName, firstLastName, secondLastName)
+        {
+            this.Rol= Rol.Id;
+            
+        }
     }
 }
