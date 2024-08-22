788bad8a95e738a95b532b2459e971049b192663
sanDaws <castrogiraldosantiago@gmail.com>
2024-08-22 09:10:36 -0500

    Feat: definition of person abstract class

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,12 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bibliosharp.Models
 {
-    public class Person
+    public abstract class Person
     {
         
+        public int id;
+        public string FirstName;
+        public string SecondName;
+        public string FirstLastName;
+        public string SecondLastName;
+
+        public Person(string FirstName,string SecondName,string FirstLastName,string SecondLastName){
+            this.FirstName=FirstName;
+            this.SecondName=SecondName;
+            this.FirstLastName=FirstLastName;
+            this.SecondLastName=SecondLastName;
+        }
     }
 }
